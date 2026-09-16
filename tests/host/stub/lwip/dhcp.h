b7dd5db27ad5acdb1d141177c7eaab55871a7735
Damien George <damien@micropython.org>
2026-09-16 10:32:30 +1000

    tests: Remove unnecessary lwip stubs.
    No longer needed now that lwIP can be disabled in the driver.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- tests/host/stub/lwip/dhcp.h
+++
@@ -1,6 +0,0 @@
-#ifndef MM_HALOW_HOSTTEST_LWIP_DHCP_H
-#define MM_HALOW_HOSTTEST_LWIP_DHCP_H
-#include <stdint.h>
-struct dhcp { uint8_t state;
-};
-#endif
--- tests/qemu/stub/lwip/dhcp.h
+++
@@ -1,6 +0,0 @@
-#ifndef HALOW_HOSTTEST_LWIP_DHCP_H
-#define HALOW_HOSTTEST_LWIP_DHCP_H
-#include <stdint.h>
-struct dhcp { uint8_t state;
-};
-#endif
